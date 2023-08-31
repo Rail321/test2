8d8aafa663f7f0ebe3207844484884870ef3f093
Rail <shakurov-98@mail.ru>
2023-08-31 14:13:05 +0300

    +add

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
-<template>
-</template>
-
 <script>
   export default {
     name: 'App'
   }
 </script>
+
+<script setup>
+  import { ref } from 'vue'
+  
+  const todos = ref( {
+    list: [],
+    get() {
+      fetch( 'https://jsonplaceholder.typicode.com' + '/todos' + '?' + ( new URLSearchParams( {
+        _sort: 'id',
+        _order: 'desc',
+        _limit: 1,
+        _page: 2,
+        userId: 9,
+        completed: false
+      } ) ) )
+        .then( r => r.json() )
+        .then( d => ( this.list = d ) )
+    }
+  } )
+
+  todos.value.get()
+</script>
+
+<template>
+  <pre>{{ todos.list }}</pre>
+</template>

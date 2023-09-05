8d74e5f2d6a7e55e203b3ee064714b548acff88c
Rail <shakurov-98@mail.ru>
2023-09-05 16:44:12 +0300

    +add

--- src/views/index.vue
+++ src/views/index.vue
@@ -5,15 +5,9 @@
 </script>
 
 <script setup>
-  import VInput from '../components/v/input.vue';
 </script>
 
 <template>
-  <div class="container px-4 py-5">
-    <div>
-      <v-input />
-    </div>
-  </div>
 </template>
 
 <style>
